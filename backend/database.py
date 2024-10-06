e793e9438d44d2f57e7c2f614bceb16228ab12b9
ptk18 <phyoethikhin143@gmail.com>
2024-10-06 22:12:08 +0700

    migrate database from sqlite to mysql

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
 from databases import Database
 
-DATABASE_URL = "sqlite:///./data.db"  # SQLite URL for local database
-
+#DATABASE_URL = "sqlite:///./data.db"  # SQLite URL for local database
+DATABASE_URL = "mysql+pymysql://root:ptk18@db:3306/QuizGeneratorDB"
+ 
 # Connect to the SQLite database
 database = Database(DATABASE_URL)
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 # Session Local to handle interactions with the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/models.py
+++ backend/models.py
@@ -6,9 +6,9 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)  # Make sure this is defined
+    name = Column(String(300))
+    email = Column(String(300), unique=True, index=True)
+    password = Column(String(300))  # Make sure this is defined
     
 class MyQuiz(Base):
     __tablename__ = 'quizzes'
